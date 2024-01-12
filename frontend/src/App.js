132d48785055707107f3949e322797716c02439b
natybkl <natnaelbekele142@gmail.com>
2024-01-12 12:16:29 +0300

    first draft of the frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+import React, { useState } from 'react';
+import HomePage from './HomePage';
+import TraineeHomePage from './TraineeHomePage';
+import StaffHomePage from './StaffHomePage';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  const [route, setRoute] = useState('home');
+
+  let pageContent;
+  if (route === 'home') {
+    pageContent = <HomePage setRoute={setRoute} />;
+  } else if (route === 'trainee') {
+    pageContent = <TraineeHomePage setRoute={setRoute} />;
+  } else if (route === 'staff') {
+    pageContent = <StaffHomePage setRoute={setRoute} />;
+  }
+
+  return <div>{pageContent}</div>;
+};
 
 export default App;
---
+++ frontend/src/HomePage.js
@@ -0,0 +1,26 @@
+// HomePage.js
+import React from 'react';
+import './styles.css'; // Import the shared styles
+
+const HomePage = ({ setRoute }) => {
+  const handleTraineeButtonClick = () => {
+    setRoute('trainee');
+  };
+
+  const handleStaffButtonClick = () => {
+    const password = prompt('Enter password:');
+    if (password === '1234') {
+      setRoute('staff');
+    }
+  };
+
+  return (
+    <div className="container home-page">
+      <h1>Welcome to the Home Page</h1>
+      <button onClick={handleTraineeButtonClick} className="btn trainee-btn">Trainee</button>
+      <button onClick={handleStaffButtonClick} className="btn staff-btn">Staff</button>
+    </div>
+  );
+};
+
+export default HomePage;
